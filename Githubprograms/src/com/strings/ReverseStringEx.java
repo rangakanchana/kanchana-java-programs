57198ed44a900efbd0b4cb386deb8118cd81ae39
kanchana ranga <rangakanchana@gmail.com>
2016-10-27 12:48:29 -0700

    string rev updated

--- Githubprograms/src/com/strings/ReverseStringEx.java
+++ Githubprograms/src/com/strings/ReverseStringEx.java
@@ -2,7 +2,7 @@ package com.strings;
 
 import java.util.Scanner;
 public class ReverseStringEx {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		String original;
 		String reverse="";
 		System.out.println("Enter the string to reverse:");
@@ -14,5 +14,29 @@ public class ReverseStringEx {
 		System.out.println("original string:"+original);
 		System.out.println("Reverse string is "+reverse);
 		sc.close();
+		}*/
+	public static void getReverseString(){
+		
+		String original;
+		String rev = "";
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("enter the string:");
+	
+	 original = sc.nextLine();
+	
+	for(int i =original.length()-1;i>=0;i--){
+		
+		rev = rev+original.charAt(i);
 		}
-        }
+	System.out.println("reversed string is "+rev);
+
+	sc.close();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		ReverseStringEx.getReverseString();
+	}
+	}
